f591bdb1427a3b271122abcfe27c1fc09fe198e1
Utkarsh Maurya <maurya124421@gmail.com>
2026-09-08 11:50:20 +0530

    Fix Vercel allowed hosts and production debug

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,9 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zewc8%%mdvc%hopev%5bu6i+vxmiq2&kzoh7!@q5r-heu##+q@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    ".vercel.app",
+]
 
 
 # Application definition
